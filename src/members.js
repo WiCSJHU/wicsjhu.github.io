d13b189882c5a2c20bf8a9c5d8bc64b122a47ee7
Jimmy Shi <jimmyshi360@gmail.com>
2018-10-24 17:12:21 -0400

    integration of react. new JS for displaying the member list

---
+++ src/members.js
@@ -0,0 +1,128 @@
+'use strict';
+
+//change this to the desired number of columns for displaying board members
+const num_columns = 4;
+
+//follow this format for adding members
+//if a member has a custom JPG image name, add an additional argument with just the name of the image! For example, for viviantsai.jpg, add the additional argument 'viviantsai'
+const members=[
+
+['Mia Boloix',
+'Sophomore CS/Math double major; enjoys all things vegan &amp; is most definitely <i>#teamVim</i>.']
+
+,['Camila Camacho',
+'Camila is the vice chair of ACM and works for the Hopkins IT department on JaySquad.']
+
+,['Coco Li',
+'Coco is a junior studying cognitive science and computer science; can be easily made happy with food.']
+
+,['Aubin Lohier',
+'CS sophomore, from NYC (QUEENS); enjoys Java, coffee, and not burning his kitchen down when he cooks.']
+
+,['Julia Oppenheim',
+'Junior studying Computer Science and Cognitive Science; passionate about education and avoiding gluten.']
+
+,['Andrew Rojas',
+'Treasurer of ACM, apple eater, bad cook. <i>West coast === best coast</i>']
+
+,['Sara Weill',
+'Sara is a sophomore CS/CE double major interested in cybersecurity; enjoys volunteering locally with APO.']
+
+,['Tony Yang',
+'Junior studying CS with an interest in networks; involved in oSTEM and local volunteering; enjoys cooking and is willing to try any food at least once.']
+
+,['Evonne Zhu',
+'Evonne is a junior majoring in Computer Science with minors in Music and Film &amp; Media Studies.']
+
+,['Aly Kruglak',
+'']
+
+,['Hailey Lee',
+'']
+
+,['Rena Liu',
+'']
+
+,['Daya Lu',
+'']
+
+,['Natalie Martinez',
+'']
+
+,['Madhu Rajmohan',
+'']
+
+,['Momo Steele',
+'']
+
+,['Gayatri Susarla',
+'']
+
+,['Sharmila Tamby',
+'']
+
+,['Nancy Wang',
+'']
+
+,['Kristin Yim',
+'']
+
+,['Bill Watson and Bailey Parker',
+'"We\'re being shipped as a unit. An absolute unit."', "billbailey"]
+
+];
+
+//takes first and last name and outputs just the first name
+function extractFirstName(nameString)
+{
+   const nameList=nameString.split(' ');
+   return nameList[0].toLowerCase();
+}
+
+//takes the 2D array of members and generates a list of HTML blocks for rendering
+const memArray=members.map((person)=>
+
+        <div data-aos= "fade-up" className="col-sm-3 mb-4">
+          <div className="card h-100 text-center">
+            <img className="card-img-top" src={"img/members/"+extractFirstName(person.length==3 ? person[2] : person[0])+".jpg"} alt=""/>
+            <div className="card-body">
+              <h4 className="card-title">{person[0]}</h4>
+              <p className="card-text">{person[1]}</p>
+            </div>
+          </div>
+        </div>
+   );
+
+//class for rendering the actual members
+class Members extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  createList() {
+    let list = [];
+
+    // generates rows of members
+    for (let i = 0; i < members.length; i+=num_columns) {
+
+        var row=[];
+
+        //generating a row of members
+        for(let j=0; j<num_columns; j++)
+            if(i+j<members.length)
+                row.push(memArray[i+j]);
+
+        list.push(<div className="row">{row}</div>);
+    }
+    return list;
+  }
+
+  render() {
+    return (
+    <div>{this.createList()}</div>
+    );
+  }
+}
+
+//React rendering the board members
+ReactDOM.render(<Members/>, document.getElementById('members_container'));
